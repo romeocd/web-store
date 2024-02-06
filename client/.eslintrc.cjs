8ea05644a98ac9795a2960199512bdec10a0cbfc
schorij23 <schorij23@gmail.com>
2024-02-06 01:54:37 -0500

    pages setup component folder

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -13,8 +13,9 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
-      'warn',
+      'off',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
   },
 }
