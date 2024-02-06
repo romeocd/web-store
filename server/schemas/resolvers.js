b3c98622a2499adacf7763aed2b618543dba4553
schorij23 <schorij23@gmail.com>
2024-02-05 19:39:41 -0500

    start resolvers and category

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,16 @@
-const { User, Order, Product} = require('../models');
-const { signToken } = require('../utils/auth');
+const { User, Order, Category, Product} = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+const stripe = require('stripe');
+
+const resolvers = {
+    Query: {
+        categories: async () => {
+            return await Category.find();
+        },
+    }
+
+
+}
 
 
 module.exports = resolvers;
