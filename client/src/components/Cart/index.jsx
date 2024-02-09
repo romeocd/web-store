f837f2053d732305ed1da41ea7071859eb9c991d
romeo <rdumlao07@gmail.com>
2024-02-08 21:17:42 -0500

    update loadStripe with publishable key

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -9,7 +9,7 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_51OhiupLfVGUnVpqrWnXHS8tnmVDipUn5TJbASpzWFZaKnAelKh15OKhYXi8FTJorn1KRU5dKDW261Qln0e7CGkEH00ITto7dum');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
