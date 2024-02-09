324bac37f327eecdfcd183c95fc10cc2461c5623
romeo <rdumlao07@gmail.com>
2024-02-08 20:34:59 -0500

    install dotenv

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { User, Order, Category, Product} = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
-const stripe = require('stripe')('sk-test_4eC39HqLyjWDarjtT1zdp7dc'); // Replace with your Stripe secret API key
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc'); // Replace with your Stripe secret API key
 
 
 const resolvers = {
@@ -78,43 +78,7 @@ const resolvers = {
           return { session: session.id };
         },
       },
-        // checkout: async (parent, args, context) => {
-        //     const url = new URL(context.headers.referer).origin;
-        //     const order = new Order({ products: args.products });
-        //     const line_items = [];
-
-        //     const { products } = await order.populate('products');
-
-        //     for (let i = 0; i < products.length; i++) {
-          
-    //             const product = await stripe.products.create({
-    //               name: products[i].name,
-    //               description: products[i].description,
-    //               images: [`${url}/images/${products[i].image}`]
-    //             });
-              
-    //             const price = await stripe.prices.create({
-    //               product: product.id,
-    //               unit_amount: products[i].price * 100,
-    //               currency: 'usd',
-    //             });
-                
-    //             line_items.push({
-    //               price: price.id,
-    //               quantity: 1
-    //             });
-    //         }
-    //         const session = await stripe.checkout.sessions.create({
-    //             payment_method_types: ['card'],
-    //             line_items,
-    //             mode: 'payment',
-    //             success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-    //             cancel_url: `${url}/`
-    //           });
-        
-    //           return { session: session.id };
-    //         }
-    // },
+      
     Mutation: {
         // Mutation to create a new user and return a token
         addUser: async (parent, args) => {
